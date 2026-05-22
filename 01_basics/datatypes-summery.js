21038118ff03536a956ad4ca9577798bccc81b42
Rohit Ranjan <rohittra2y@gmail.com>
2026-05-22 17:30:53 +0000

    change in location of file

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : string, Number, Boolearn,null , symbol, BigInt
+
+
+const Score = 100
+const scoreValue = 100.3
+
+const isLoggedIn= false
+const OutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+//const bigNumber =344474474474449n
+
+// Reference ( Non primitive)
+
+// Array , Objects, FUNCTION
+
+const heros =["doga", "naagraaj", "dhruva"];
+let myObj = {
+    name: "paggo",
+    age : 25,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber);
