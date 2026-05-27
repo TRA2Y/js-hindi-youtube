c3127da3ac1c27026dfbcfe5b1cece4be02fecfb
Rohit Ranjan <rohittra2y@gmail.com>
2026-05-27 17:32:35 +0000

    scope this and arrow

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,53 @@
+const user = {
+    username:"paggo",
+    price:999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "paggo"
+//     console.log(this);
+    
+// }
+// chai()
+
+// const chai = function(){
+    // let username = "paggo"
+    // console.log(this.username);
+    
+// }
+
+const chai = () => {
+    let username = "paggo"
+    console.log(this);
+}
+// chai()
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1, num2) =>  num1 + num2 
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2)  => ({username: "paggo"})
+
+
+console.log(addTwo(3, 4));
+
+// myArray = [2, 5, 3, 7, 8]
+// myArray.forEach()
+
