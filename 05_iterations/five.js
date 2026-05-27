1a32bd5e3b4cce530ca255b691b057e955a50f9a
Rohit Ranjan <rohittra2y@gmail.com>
2026-05-27 17:30:42 +0000

    scope this and arrow

---
+++ 05_iterations/five.js
@@ -0,0 +1,51 @@
+
+const coding = ["js", "ruby", "python", "java", "cpp"]
+
+// coding.forEach( function name (val) {
+//     console.log(val);
+    
+// })
+
+// coding.forEach( (item) => {
+//   console.log(item);
+  
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach((Item, index, arr)=>{
+// console.log(Item,index, arr);
+
+// })
+
+const myCoding = [
+    {
+        languageName:" javascript",
+        languageFileName: "java"
+    },
+
+
+    {
+        languageName:" python",
+        languageFileName: "py"
+        
+    },
+
+
+    {
+        languageName:" java",
+        languageFileName: "java"
+        
+    },
+
+
+]
+ myCoding.forEach((item) => {
+
+    console.log(item.languageName);
+    
+ })
