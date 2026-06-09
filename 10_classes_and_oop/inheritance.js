a1ac067aac5af77bd3cf740b65f14b3b956fd177
Rohit Ranjan <rohittra2y@gmail.com>
2026-06-09 02:29:12 +0000

    Bind keyword in javascript

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+        
+    }
+}
+
+class teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+
+
+    }
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+const chai = new teacher("chai", "chai@teacher.com", "123")
+
+chai.addCourse()
+const masalachai = new User("masalachai")
+
+masalachai.logMe()
+
+console.log(chai === masalachai);
